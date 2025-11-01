f2306c118fd4c19714c28796ba722cdffdcb19fc
joseignaciolopezsaiz-stack <jose.ignacio.lopez.saiz@gmail.com>
2025-11-01 15:34:47 +0100

    Add files via upload

--- colas.py
+++ colas.py
@@ -6,19 +6,13 @@ class Cola:
     self.cola.append(elemento)
 
   def nueva_cola(self):
-    if self.vacia():
-      return "cola vacia"
-    else:
-      self.cola.pop(0)
-      return self.cola
+    self.cola.pop(0)
+    return self.cola
       
 
   def sacar_cola(self):
     print("_____<<>>_____")
-    if self.vacia():
-      return "cola vacia"
-    else:
-      return self.cola[0]
+    return self.cola[0]
 
   def vacia(self):
     #print("/////",len(self.cola) == 0)
@@ -36,30 +30,40 @@ mya.añadir("S")
 mya.añadir("D")
 
 print("Cola: ", mya.cola)
-print("Elemen sacado cola", mya.sacar_cola())
+print("Elemen a elim cola", mya.sacar_cola())
 print("Nueva cola: ", mya.nueva_cola())
 print("cola vacia: ", mya.vacia())
 print("Tamaño: ", mya.tamaño())
 mya.añadir("E")
-print("Elem sacado cola", mya.sacar_cola())
+print("elem añad E")
+print("Elem a elim cola", mya.sacar_cola())
 print("Nueva cola: ", mya.nueva_cola())
 print("cola vacia: ", mya.vacia())
 print("Tamaño: ", mya.tamaño())
-print("Elem sacado cola:", mya.sacar_cola())
+print("Elem a elim cola:", mya.sacar_cola())
 print("Nueva cola: ", mya.nueva_cola())
 print("cola vacia: ", mya.vacia())
 print("Tamaño: ", mya.tamaño())
 mya.añadir("F")
+print("Elem añad F")
 
-print("Elem sacado cola:", mya.sacar_cola())
+print("Elem a elim cola:", mya.sacar_cola())
 print("Nueva cola: ", mya.nueva_cola())
 print("cola vacia: ", mya.vacia())
 print("Tamaño: ", mya.tamaño())
-print("Elem sacado cola:", mya.sacar_cola())
+print("Elem a elim cola:", mya.sacar_cola())
 print("Nueva cola: ", mya.nueva_cola())
 print("cola vacia: ", mya.vacia())
 print("Tamaño: ", mya.tamaño())
-print("Elem sacado cola:", mya.sacar_cola())
+print("Elem a elim cola:", mya.sacar_cola())
 print("Nueva cola: ", mya.nueva_cola())
 print("cola vacia: ", mya.vacia())
 print("Tamaño: ", mya.tamaño())
+mya.añadir("G")
+print("Elem añad G")
+mya.añadir("H")
+print("Elem añad H")
+print("Elem a elim cola",mya.sacar_cola())
+print("Nueva cola: ", mya.nueva_cola())
+print("Cola vacia",mya.vacia())
+print("tamaño",mya.tamaño())
